15b43b97d32f4f6ca47aadfde10704b5e79e8195
AxelMonge1 <axel.monge262799@potros.itson.edu.mx>
2026-04-25 17:48:12 -0600

    Grafo terminado

--- Grafo_Oaxaca/src/main/java/elementos/Grafo.java
+++ Grafo_Oaxaca/src/main/java/elementos/Grafo.java
@@ -4,7 +4,9 @@
  */
 package elementos;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  *
@@ -37,5 +39,48 @@ public class Grafo {
             }
         }
         return nodo;
+    }
+    
+    public List<Nodo> buscarRuta(String origen, String destino){
+        Nodo nodoOrigen = buscarNodo(origen);
+        Nodo nodoDestino = buscarNodo(destino);
+        List<Nodo> ruta = new ArrayList<>();
+        if(nodoOrigen == null || nodoDestino == null){
+            throw new IllegalArgumentException("No existe el origen o el destino (hacer debug)");
+        }
+        if(buscarRutaDFS(ruta, nodoOrigen, nodoDestino)){
+            return ruta;
+        }else{
+            return null;
+        }
+    }
+    
+    private boolean buscarRutaDFS(List<Nodo> ruta, Nodo nodoOrigen, Nodo nodoDestino){
+        ruta.add(nodoOrigen);
+        if(nodoOrigen.getMunicipio().equals(nodoDestino.getMunicipio())){
+            return true;
+        }
+        Stack<Nodo> pila = new Stack<>();
+        ArrayList<Nodo> nodosVisitados = new ArrayList<>();
+        pila.add(nodoOrigen);
+        while(!pila.isEmpty()){
+            Nodo actual = pila.pop();
+            if(nodosVisitados.contains(actual)){
+                continue;
+            }
+            if(actual.equals(nodoDestino)){
+                ruta.addAll(pila);
+                ruta.add(nodoDestino);
+                return true;
+            }else{
+                nodosVisitados.add(actual);
+                for(Nodo n : actual.getNodosAdyacentes()){
+                    if(!pila.contains(n)){
+                        pila.add(n);
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
